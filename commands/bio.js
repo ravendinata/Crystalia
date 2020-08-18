18ebd3e82db911e81f0c656fdfd6a500c781859d
ravendinata <61657795+ravendinata@users.noreply.github.com>
2020-08-18 16:52:26 +0800

    Modified argument error error handling
    - Added extra message as an error statement follow up
    - Cleaned up old code
    - Added capitalization to group name in error statements

--- commands/bio.js
+++ commands/bio.js
@@ -8,6 +8,7 @@ var pool = mysql.createPool({
     database: 'heroku_774353f79cb52ed'
 });
 
+//Original connection method
 /* con.connect(function(err) {
     if (err) throw err;
     console.log("Connected to Database!");
@@ -30,13 +31,15 @@ module.exports = {
             {
                 if (err) 
                 {
-                    message.channel.send(`Argument Error: Cannot Find Specified Group!\nGroup: ${args[0]}`);
+                    message.channel.send(`Argument Error: Cannot find specified group ${args[0].toUpperCase()}!`);
+                    message.channel.send(`Only Japan groups (AKB48, HKT48, NGT48, NMB48, SKE48, STU48) are available at the moment.`);
                     return;
                 };
 
                 if (rows[0] == undefined)
                 {
-                    message.channel.send(`Argument Error: Cannot Find Member from the Specified Group!\nGroup: ${args[0]}\nMember Name: ${args[1]}`);
+                    message.channel.send(`Argument Error: Cannot find ${args[1]} from the Group ${args[0].toUpperCase()}!`);
+                    message.channel.send(`The member may not be available yet or there might be spelling error(s).`);
                     return;
                 }
 
@@ -61,125 +64,6 @@ module.exports = {
                     
                 return message.channel.send(bio);
             })
-    })
-        /* else if (args[0] === 'hkt48')
-        {
-            const color = '#000000';
-
-            if (args[1] === 'unjohirona')
-            {
-                const bio = new Discord.MessageEmbed()
-                    .setColor(color)
-                    .setTitle(`Unjo Hirona's (運上弘菜) Biography`)
-                    .addFields(
-                        { name: 'Nickname:', value: 'Nappi (なっぴ)'},
-                        { name: 'Birthdate:', value: 'August 9, 1998'},
-                        { name: 'Birthplace:', value: 'Obira, Hokkaido, Japan'},
-                        { name: 'Blood Type:', value: 'B'},
-                        { name: 'Height:', value: '155cm'},
-                        { name: 'Group:', value: 'HKT48'},
-                        { name: 'Team:', value: 'Team KIV'}
-                    )
-                    .setImage('https://vignette.wikia.nocookie.net/akb48/images/8/84/Unjo_Hirona_HKT48_2019.jpg')
-
-                return message.channel.send(bio);
-            }
-        }
-        else if (args[0] === 'ske48')
-        {
-            const color = '#f7b501';
-
-            if (args[1] === 'sudaakari')
-            {
-                const bio = new Discord.MessageEmbed()
-                    .setColor(color)
-                    .setTitle(`Suda Akari's (須田亜香里) Biography`)
-                    .addFields(
-                        { name: 'Nickname:', value: 'Akarin (あかりん)\nDasu (だす)'},
-                        { name: 'Birthdate:', value: 'October 31, 1991'},
-                        { name: 'Birthplace:', value: 'Nagoya, Aichi, Japan'},
-                        { name: 'Blood Type:', value: 'A'},
-                        { name: 'Height:', value: '159cm'},
-                        { name: 'Group:', value: 'SKE48'},
-                        { name: 'Team:', value: 'Team E'}
-                    )
-                    .setImage('http://stage48.net/wiki/images/0/00/SudaAkariE2019.jpg')
-
-                return message.channel.send(bio);
-            }
-        }
-        else if (args[0] === 'nmb48')
-        {
-            const color = '#eb9d47';
-
-            if (args[1] === 'umeyamacocona')
-            {
-                const bio = new Discord.MessageEmbed()
-                    .setColor(color)
-                    .setTitle(`Umeyama Cocona's (梅山恋和) Biography`)
-                    .addFields(
-                        { name: 'Nickname:', value: 'Cocona (ココナ)'},
-                        { name: 'Birthdate:', value: 'August 7, 2003'},
-                        { name: 'Birthplace:', value: 'Osaka, Japan'},
-                        { name: 'Blood Type:', value: 'O'},
-                        { name: 'Height:', value: '157cm'},
-                        { name: 'Group:', value: 'NMB48'},
-                        { name: 'Team:', value: 'Team BII'}
-                    )
-                    .setImage('http://stage48.net/wiki/images/2/26/UmeyamaCoconaBII2019.jpg')
-
-                return message.channel.send(bio);
-            }
-        }
-        else if (args[0] === 'stu48')
-        {
-            const color = '#d0e7f9';
-
-            if (args[1] === 'takinoyumiko')
-            {
-                const bio = new Discord.MessageEmbed()
-                    .setColor(color)
-                    .setTitle(`Takino Yumiko's (瀧野由美子) Biography`)
-                    .addFields(
-                        { name: 'Nickname:', value: 'Yumirin (ゆみりん)'},
-                        { name: 'Birthdate:', value: 'September 24, 1997'},
-                        { name: 'Birthplace:', value: 'Yamaguchi, Japan'},
-                        { name: 'Blood Type:', value: 'O'},
-                        { name: 'Height:', value: '164cm'},
-                        { name: 'Group:', value: 'STU48'},
-                        { name: 'Team:', value: 'STU'}
-                    )
-                    .setImage('http://stage48.net/wiki/images/f/f6/TakinoYumikoSTU2019.jpg')
-
-                return message.channel.send(bio);
-            }
-        }
-        else if (args[0] === 'ngt48')
-        {
-            const color = '#ff0000';
-
-            if (args[1] === 'hommahinata')
-            {
-                const bio = new Discord.MessageEmbed()
-                    .setColor(color)
-                    .setTitle(`Homma Hinata's (本間日陽 ) Biography`)
-                    .addFields(
-                        { name: 'Nickname:', value: 'Hinatan (ひなたん)\nHinaHina (ひなひな) '},
-                        { name: 'Birthdate:', value: 'November 10, 1999'},
-                        { name: 'Birthplace:', value: 'Murakami, Niigata, Japan'},
-                        { name: 'Blood Type:', value: 'B'},
-                        { name: 'Height:', value: '159cm'},
-                        { name: 'Group:', value: 'NGT48'},
-                        { name: 'Team:', value: '1st Generation'}
-                    )
-                    .setImage('http://stage48.net/wiki/images/f/fa/HommaHinata2019.jpg')
-
-                return message.channel.send(bio);
-            }
-        } */
-
-        /* message.channel.send('Argument Error: Cannot Find Member from the Specified Group!');
-        message.channel.send(`Group: ${args[0]}`);
-        message.channel.send(`Member Name: ${args[1]}`); */
+        })
     },
 };
