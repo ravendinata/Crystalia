ddb940a0a669e76a327502da35416ad8f305d32a
ravendinata <61657795+ravendinata@users.noreply.github.com>
2020-10-12 22:32:25 +0800

    Update Catchphrase to Database
    Changed catchphrase calling method to use database instead of pre-written in code.

--- commands/catchphrase.js
+++ commands/catchphrase.js
@@ -1,8 +1,61 @@
+const Discord = require('discord.js');
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'bf0b2d2a04cd04',
+    password: 'c82ad2e3',
+    database: 'heroku_774353f79cb52ed'
+});
+
 module.exports = {
 	name: 'catchphrase',
     description: 'Displays catchphrase of selected member',
     execute(message, args) {
-        if (args[0] === 'akb48')
+
+        pool.getConnection((err, con) => 
+        {
+            if (args[0] === 'akb48') var color = '#ff69b3';
+            else if (args[0] === 'hkt48') var color = '#000000';
+            else if (args[0] === 'ngt48') var color = '#ff0000';
+            else if (args[0] === 'nmb48') var color = '#eb9d47';
+            else if (args[0] === 'ske48') var color = '#f7b501';
+            else if (args[0] === 'stu48') var color = '#d0e7f9';
+
+            con.query(`SELECT * FROM ` + args[0]+ ` WHERE short='` + args[1] + `'`, function (err, rows)
+            {
+                if (err) 
+                {
+                    message.channel.send(`Argument Error: Cannot find specified group ${args[0].toUpperCase()}!`);
+                    message.channel.send(`Only Japan groups (AKB48, HKT48, NGT48, NMB48, SKE48, STU48) are available at the moment.`);
+                    return;
+                };
+
+                if (rows[0] == undefined)
+                {
+                    message.channel.send(`Argument Error: Cannot find ${args[1]} from the Group ${args[0].toUpperCase()}!`);
+                    message.channel.send(`The member may not be available yet or there might be spelling error(s).`);
+                    return;
+                }
+
+                const result = JSON.stringify(rows[0]);
+                const data = JSON.parse(result);
+
+                const bio = new Discord.MessageEmbed()
+                .setColor(color)
+                .setTitle(`${data.name}'s (${data.name_kanji}) Catchphrase`)
+                .addFields(
+                    { name: 'Catchphrase:', value: data.catchphrase }
+                )
+                .setImage(data.img_url)
+
+                con.release();
+                    
+                return message.channel.send(bio);
+            })
+        })
+
+        /* if (args[0] === 'akb48')
         {
             if (args[1] === 'oguriyui')
             {
@@ -27,6 +80,6 @@ module.exports = {
 
         message.channel.send('Cannot Find Member from the Specified Group!');
         message.channel.send(`Group: ${args[0]}`);
-        message.channel.send(`Member Name: ${args[1]}`);
+        message.channel.send(`Member Name: ${args[1]}`); */
     },
 };
