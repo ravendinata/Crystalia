b8416359e988a893663ffb1288bfe875899f64d5
Raven Limadinata <raven.limadinata@outlook.com>
2023-04-28 23:56:18 +0800

    Migrated 'ping' command to slash command
    - Updated the command to use slash command and new interaction method
    - Added latency information to the reply so the command becomes more informative

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,18 @@
-module.exports = {
-	name: 'ping',
-    description: 'Ping!',
-    execute(message, args) {
-        message.reply('pong');
-        message.channel.send('pong');
-    },
+const { SlashCommandBuilder } = require('discord.js');
+
+console.info("Ping Module Initialized!");
+
+module.exports = 
+{
+    name: 'ping',
+    description: 'Pings the bot',
+	data: new SlashCommandBuilder()
+			.setName('ping')
+			.setDescription('Pings the bot to check if it is online and responsive'),
+
+	async execute(interaction) 
+	{
+        console.info(`Ping command received from ${interaction.user.tag}`);
+		interaction.reply(`Pong! Latency is ${Date.now() - interaction.createdTimestamp}ms.`);
+	}
 };
